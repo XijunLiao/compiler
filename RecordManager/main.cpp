da04715482cc70255b26a484cb1632b17af307d8
XijunLiao <237859682@qq.com>
2018-06-24 11:30:45 +0800

    main更新了测试

--- RecordManager/main.cpp
+++ RecordManager/main.cpp
@@ -6,33 +6,82 @@
 
 using namespace std;
 
-bool judge(BaseData* data){
-    int age = ((int*)data->getBytes())[0];
-    return age>20;
+bool judge(char * data){
+    return strcmp(data, "Laplace") == 0;
 }
-void setBitMap(char * source, int size, int position);
 
 int main() {
 
-    char id[11] = "3160104339";
-    char name[10] = "Jack Ma";
-    int age = 20;
-    vector<Column> attrs = {
-            /* 依次是属性名，类型，大小，约束 */
-            make_tuple("id", DB_STRING, 11, PRIMARY_KEY),
+    char id[10] = "00004";
+    char name[10] = "Laplace";
+    int age = NULL;
+    char major[10] = "Math";
+
+    vector<Column> col = {
+            make_tuple("id", DB_STRING, 10, PRIMARY_KEY),
             make_tuple("name", DB_STRING, 10, NONE),
-            make_tuple("age", DB_INT, 4, NONE)
+            make_tuple("age", DB_INT, 4, NONE),
+            make_tuple("major", DB_STRING, 10, NONE)
     };
 
-    Table *student = new Table(1, "student", attrs);
+    auto table = new Table(1, "student", col);
 
-    vector<char *> record = {
-            id,
-            name,
-            (char*)&age
+    vector<char *> arr = {
+            id, name, nullptr, major
     };
-    student->insertRecord(record);
+
+    vector<string> fields = {"id", "name"};
+
+    auto res = table->query(
+            fields,
+            *new string("name"),
+            judge
+    );
+
+    BaseData* data = (*res[0].find("id")).second;
+
+    string LaplaceId = (dynamic_cast<StringData*> (data))->getString();
+
+    cout << LaplaceId << endl;
 
     return 0;
 
 }
+
+/*
+    char id[10] = "00001";
+    char name[10] = "Jack";
+    int age = 18;
+    char major[10] = "Comp.Sci";
+
+    char id[10] = "00002";
+    char name[10] = "Franklin";
+    int age = 20;
+    char major[10] = "Comp.Sci";
+
+    char id[10] = "00003";
+    char name[10] = "Winston";
+    int age = 22;
+    char major[10] = "History";
+
+    char id[10] = "00004";
+    char name[10] = "Laplace";
+    int age = NULL;
+    char major[10] = "Math";
+
+    vector<Column> col = {
+            make_tuple("id", DB_STRING, 10, PRIMARY_KEY),
+            make_tuple("name", DB_STRING, 10, NONE),
+            make_tuple("age", DB_INT, 4, NONE),
+            make_tuple("major", DB_STRING, 10, NONE)
+    };
+
+    auto table = new Table(1, "student", col);
+
+    table->insertRecord(*new vector<char *> {
+        id, name, (char*)&age, major
+    });
+
+    auto manager = table->getPageManager();
+    manager.flush();
+*/
